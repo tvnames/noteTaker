89438ecf838677313ad1170a78e44b7598bdcd76
Andrew Parsons <77991510+tvnames@users.noreply.github.com>
2021-04-19 09:47:15 -0700

    Fixed redundancy and api routes

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 
 const path = require("path");
-const db = require("./Public/db/db.json");
-const createhtmlApiroutes = ("./routes/htmlApiRoutes");
-
+const db = require("./db/db.json");
+const createhtmlApiRoutes = require("./routes/htmlApiRoutes");
+const createApiRoutes = require("./routes/ApiRoutes")
 const app = express();
 const PORT = process.env.PORT || 3002;
 
@@ -11,9 +11,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "./public/notes.html")));
 
-app.use(require("./routes/ApiRoutes"));
-app.use(require("./routes/htmlApiRoutes").default);
-createhtmlApiroutes(app);
+createhtmlApiRoutes(app);
+createhtmlApiRoutes(app);
+
+
+
 
 app.listen(PORT, () => {
   console.log(`App listening on PORT: ${PORT}`);
